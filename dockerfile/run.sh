35183ab381366c02f55b792826f514d16b7e40d9
Aditya Patawari <adimania@gmail.com>
2016-01-24 15:29:49 +0530

    let us use env vars

--- dockerfile/run.sh
+++ dockerfile/run.sh
@@ -6,7 +6,7 @@ then
     mv /opt/magento /var/www/html/magento
     chown -R apache:apache /var/www/html/magento && find /var/www/html/magento/ -type d -exec chmod 770 {} \; && find /var/www/html/magento/ -type f -exec chmod 660 {} \; && chmod u+x /var/www/html/magento/bin/magento
     cd /var/www/html/magento/bin
-    su -s /bin/sh apache -c './magento setup:install --base-url=http://shop.adi.im/magento/ --db-host=mysql --db-name=magento --db-user=root --db-password=my-secret-pw --admin-firstname=Magento --admin-lastname=User --admin-email=user@example.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD --timezone=America/Chicago --use-rewrites=1'
+    su -s /bin/sh apache -c './magento setup:install --base-url=$BASE_URL --db-host=$DB_HOST --db-name=$DB_NAME --db-user=$DB_USER --db-password=$DB_PASSWORD --admin-firstname=$ADMIN_FNAME --admin-lastname=$ADMIN_LNAME --admin-email=$ADMIN_EMAIL --admin-user=$ADMIN_USER --admin-password=$ADMIN_PASSWORD --use-rewrites=1'
 else 
     rm /opt/magento
     echo "Setup looks OK! Starting webserver..."
